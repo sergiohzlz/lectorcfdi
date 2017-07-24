47ecb5357a7f8ec46c414107b242fa31f600cbea
Sergio Hernández-López <sergiohzlz@gmail.com>
2017-07-24 00:27:02 -0500

    Repite RFC para emisor y receptor en caso de venir vacíos

--- extrainfo.py
+++ extrainfo.py
@@ -40,16 +40,30 @@ def extraeinfo(f):
             retiva += float(t['importe'])
 
     tc = float(comprobante.get('tipocambio',1.))
-    resumen = "{0} \t {1} \t {2} \t {3} \t {4} \t {5} \t {6} \t{7} \t {8} \t {9} \t {10} \t {11} \t {12}".format(\
-               emisor['nombre'].encode('utf8'), \
-               comprobante['fecha'].encode('utf8'), \
-               emisor['rfc'].encode('utf8'), \
-               tfd['uuid'].encode('utf8'), \
-               receptor['nombre'].encode('utf8'), \
-               receptor['rfc'].encode('utf8'), \
-               comprobante['subtotal'].encode('utf8'), \
-               str(ieps),str(iva),str(retiva),str(retisr), str(tc), \
-               comprobante['total'].encode('utf8'))
+
+    try:
+        emisornombre = emisor['nombre'].encode('utf8')
+    except:
+        emisornombre = emisor['rfc'].encode('utf8')
+
+    try:
+        receptornombre = receptor['nombre'].encode('utf8')
+    except:
+        receptornombre = receptor['rfc'].encode('utf8')
+
+    try:
+        resumen = "{0} \t {1} \t {2} \t {3} \t {4} \t {5} \t {6} \t{7} \t {8} \t {9} \t {10} \t {11} \t {12}".format(\
+                emisornombre, \
+                comprobante['fecha'].encode('utf8'), \
+                emisor['rfc'].encode('utf8'), \
+                tfd['uuid'].encode('utf8'), \
+                receptornombre, \
+                receptor['rfc'].encode('utf8'), \
+                comprobante['subtotal'].encode('utf8'), \
+                str(ieps),str(iva),str(retiva),str(retisr), str(tc), \
+                comprobante['total'].encode('utf8'))
+    except KeyError as k:
+        print("Error en {0}".f)
     return resumen
 
 
@@ -59,6 +73,9 @@ L = glob.glob('./*.xml')
 if __name__=='__main__':
     print("Emisor \t Fecha_CFDI \t RFC_Emisor \t Folio_fiscal \t Receptor \t RFC_Receptor \t Subtotal \t  IEPS \t IVA \t Ret IVA \t Ret ISR \t TC \t Total")
     for f in L:
-        rcfdi = extraeinfo(f)
+        try:
+            rcfdi = extraeinfo(f)
+        except:
+            assert "Error en archivo {0}".format(f)
         print(rcfdi)
 
