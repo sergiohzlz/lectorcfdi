eb1c152f627336073b72cb55ef44aeb17ccc469b
Sergio Hernández <sergiohzlz@gmail.com>
2021-04-14 16:17:49 -0500

    esqueleto de base sql

--- base_crea.sql
+++ base_crea.sql
@@ -1,18 +1,25 @@
-DROP TABLE IF EXISTS contribuyentes;
+DROP TABLE cfdis;
 
-CREATE TABLE contribuyentes (
-    emisor_rfc      varchar(17) primary key,
-    emisor_nombre   varchar(250),
-    emisor_fecha    date,
-    tipo            varchar(10),
-    folio_fiscal    varchar(37),
-    receptor_nombre varchar(250),
-    receptor_rfc    varchar(17) not null,
-    subtotal        float,
-    ieps            float,
-    iva             float,
-    ret_iva         float,
-    ret_isr         float,
-    tc              float,
-    total           float
+
+CREATE TABLE cfdis (
+    uuid             varchar(32) primary key,
+    emisor_nombre    text,
+    emisor_rfc       varchar(15) not null,
+    receptor_nombre  text,
+    receptor_rfc     varchar(15) not null,
+    fecha            date,
+    tipo_cambio      float default 1,
+    total            float default 0,
+    subtotal         float default 0, 
+    traslado_iva     float default 0,
+    traslado_ieps    float default 0,
+    traslado_isr     float default 0,
+    retencion_iva    float default 0,
+    retencion_isr    float default 0,
+    cfdi_moneda      varchar(3),
+    cfdi_tipo        varchar(7), 
+    cfdi_fecha_t     date,                     --fecha timbrado
+    cfdi_sello       text,                     --sello cfdi
+    cfdi_cert        text,                     --certificado cfdi
+    cfdi_version     varchar(3)                --version cfdi
 );
